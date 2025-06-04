ce9727b370d9c73768f8b3148c09329a0a8d55e3
JerSbs <jeremysbs.97@gmail.com>
2025-06-04 23:29:59 +0700

    Add the order_id column to the payment table

--- db/migrations/000004_create_payment_table.up.sql
+++ db/migrations/000004_create_payment_table.up.sql
@@ -1,9 +1,11 @@
 CREATE TABLE payment (
     payment_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
+    order_id INT,
     payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     amount DECIMAL(12,2) NOT NULL,
     payment_method VARCHAR(50),
     status VARCHAR(30) DEFAULT 'pending',
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
 );
